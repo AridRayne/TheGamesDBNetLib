f153e5b8123d5641ea7cc736ba06af3fdf7d329a
AridRayne <aridrayne@gmail.com>
2013-11-04 21:14:34 -0800

    Added and updated ratingClass.

---
+++ TheGamesDBNetLib/src/com/AridRayne/thegamesdb/lib/ratingClass.java
@@ -0,0 +1,12 @@
+package com.AridRayne.thegamesdb.lib;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Path;
+import org.simpleframework.xml.Root;
+
+@Root(name="Data")
+public class ratingClass {
+	@Path("game")
+	@Element
+	double Rating;
+}
