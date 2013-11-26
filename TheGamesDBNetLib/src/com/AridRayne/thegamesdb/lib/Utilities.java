69b43f80db5c9568e089aa2cac89c19e99fa8454
AridRayne <aridrayne@gmail.com>
2013-11-25 23:20:21 -0800

    Removed a couple unused imports in Utilities.java.

--- TheGamesDBNetLib/src/com/AridRayne/thegamesdb/lib/Utilities.java
+++ TheGamesDBNetLib/src/com/AridRayne/thegamesdb/lib/Utilities.java
@@ -4,12 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 
-import org.apache.commons.lang3.StringEscapeUtils;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
 
